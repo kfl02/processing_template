8148533ca153200a2b30082cafae6e9b215a52cb
Karsten Fleischer <karsten.fleischer@hrs.de>
2024-02-06 13:57:11 +0100

    all fields now in base class, have to be inited from derived constructor

--- processing_template/ImageLoader.java
+++ processing_template/ImageLoader.java
@@ -1,17 +1,36 @@
 package processing_template;
 
 import processing.core.PApplet;
+import processing.core.PImage;
 
 public class ImageLoader {
-	private PApplet app;
+    protected PApplet app;
+    public int num_images;
+    public PImage[] images = null;
+    public String[] image_names = null;
+    public int[] x_offs = null;
+    public int[] y_offs = null;
+    public int num_tile_images;
+    public PImage[][] tile_images = null;
+    public String[] tile_image_names = null;
+    public int[] tile_image_num = null;
+    public int[][] tile_x_offs = null;
+    public int[][] tile_y_offs = null;
 
-	public ImageLoader(PApplet app) {
-		this.app = app;
-	}
+    protected ImageLoader() {
+    }
 
-	public void loadImages() {
-	}
+    public ImageLoader(PApplet app, int numImages, int numTileImages) {
+        this.app = app;
+        num_images = numImages;
+        num_tile_images = numTileImages;
 
-	public void loadTileImages() {
-	}
+        if (num_images > 0) {
+            images = new PImage[num_images];
+        }
+
+        if (num_tile_images > 0) {
+            tile_images = new PImage[num_tile_images][];
+        }
+    }
 }
