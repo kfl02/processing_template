d51fc34b60069deef0515342a36c75e348148b14
Karsten Fleischer <karsten.fleischer@hrs.de>
2024-02-06 13:58:25 +0100

    indentation

--- processing_template/ProcessingAppBase.java
+++ processing_template/ProcessingAppBase.java
@@ -9,109 +9,104 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 public class ProcessingAppBase extends PApplet {
-	private VideoExport videoExport;
-	private boolean video_running = false;
-	protected boolean animate = false;
-	protected ImageLoader img;
+    private VideoExport videoExport;
+    private boolean video_running = false;
+    protected boolean animate = false;
+    protected ImageLoader img;
 
-	public class Point extends verlet.Point {
-		public Point(PVector pos) {
-			super(pos);
-		}
+    public class Point extends verlet.Point {
+        public Point(PVector pos) {
+            super(pos);
+        }
 
-		public Point(PVector pos, PVector v) {
-			super(pos, v);
-		}
+        public Point(PVector pos, PVector v) {
+            super(pos, v);
+        }
 
-		public Point(float x, float y) {
-			super(x, y);
-		}
+        public Point(float x, float y) {
+            super(x, y);
+        }
 
-		public Point(float x, float y, float vx, float vy) {
-			super(x, y, vx, vy);
-		}
+        public Point(float x, float y, float vx, float vy) {
+            super(x, y, vx, vy);
+        }
 
-		@Override
-		public void draw() {
-			if (draw) {
-				circle(pos.x, pos.y, radius * 2);
-			}
-		}
-	}
+        @Override
+        public void draw() {
+            if (draw) {
+                circle(pos.x, pos.y, radius * 2);
+            }
+        }
+    }
 
-	public class Line extends verlet.Line {
-		public Line(Point p1, Point p2) {
-			super(p1, p2);
-		}
+    public class Line extends verlet.Line {
+        public Line(Point p1, Point p2) {
+            super(p1, p2);
+        }
 
-		public Line(Point p1, Point p2, float length) {
-			super(p1, p2, length);
-		}
+        public Line(Point p1, Point p2, float length) {
+            super(p1, p2, length);
+        }
 
-		@Override
-		public void draw() {
-			if (draw) {
-				line(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y);
-			}
-		}
-	}
+        @Override
+        public void draw() {
+            if (draw) {
+                line(p1.pos.x, p1.pos.y, p2.pos.x, p2.pos.y);
+            }
+        }
+    }
 
-	@Override
-	public void setup() {
-		img = new ImageLoader(this);
+    @Override
+    public void setup() {
+        videoExport = new VideoExport(this, getClass().getName() + "_"
+                + LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ".mp4");
 
-		img.loadImages();
-		img.loadTileImages();
+        videoExport.setQuality(70, 128);
+        videoExport.setFrameRate(frameRate);
+    }
 
-		videoExport = new VideoExport(this, getClass().getName() + "_"
-				+ LocalDateTime.now().format(DateTimeFormatter.ISO_LOCAL_DATE_TIME) + ".mp4");
+    @Override
+    public void draw() {
+        if (animate) {
+            animate();
+        }
 
-		videoExport.setQuality(70, 128);
-		videoExport.setFrameRate(frameRate);
-	}
+        if (video_running) {
+            videoExport.saveFrame();
+        }
+    }
 
-	@Override
-	public void draw() {
-		if (animate) {
-			animate();
-		}
+    void animate() {
+    }
 
-		if (video_running) {
-			videoExport.saveFrame();
-		}
-	}
+    @Override
+    public void keyPressed() {
+        switch (key) {
+            case '#':
+                if (!video_running) {
+                    videoExport.startMovie();
 
-	void animate() {
-	}
+                    video_running = true;
 
-	@Override
-	public void keyPressed() {
-		switch (key) {
-		case '#':
-			if (!video_running) {
-				videoExport.startMovie();
+                    return;
+                } else {
+                    video_running = false;
 
-				video_running = true;
+                    videoExport.endMovie();
 
-				return;
-			} else {
-				video_running = false;
+                    exit();
+                }
+                break;
 
-				videoExport.endMovie();
+            case 'm':
+                animate = !animate;
+                break;
 
-				exit();
-			}
-			break;
-
-		case 'm':
-			animate = !animate;
-			break;
-
-		case '.':
-			if (!animate) {
-				animate();
-			}
-			break;
-		}
-	}
+            case '.':
+                if (!animate) {
+                    animate();
+                }
+                break;
+        }
+    }
 }
